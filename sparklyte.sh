bbc3cbdfbaf7bba210bdb06ed9fe9608f2a8aa6d
Gabriel Claramunt <gabriel.claramunt@gmail.com>
2015-08-05 16:11:10 -0300

    Update sparklyte.sh

--- sparklyte.sh
+++ sparklyte.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Welcome to SparkLyte"
+echo " ____                   _    _          _        "
+echo "/ ___| _ __   __ _ _ __| | _| |   _   _| |_ ___  "
+echo "\___ \| '_ \ / _` | '__| |/ / |  | | | | __/ _ \ "
+echo " ___) | |_) | (_| | |  |   <| |__| |_| | ||  __/ " 
+echo "|____/| .__/ \__,_|_|  |_|\_\_____\__, |\__\___| "
+echo "      |_|                         |___/          " 
 #just call the Scala repl and analyze your data;-)
 scala
